6339ef7c7f203d5ca5e21f73c7c21ed93017ae0c
ana-oprea <anaop3212@gmail.com>
2021-10-13 22:37:52 +0300

    FIles

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2OVSSfvCe4Pn0Q9R_H2TAVu906EOHMWI",
+    authDomain: "netflix-clone-ec81d.firebaseapp.com",
+    projectId: "netflix-clone-ec81d",
+    storageBucket: "netflix-clone-ec81d.appspot.com",
+    messagingSenderId: "254984532360",
+    appId: "1:254984532360:web:8ff4a1e016b04ed6e94a3d"
+  };
